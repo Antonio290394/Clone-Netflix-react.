89b3b6e6d3589b71e12156c6edf5f024ea3540aa
Antonio290394 <122730142+Antonio290394@users.noreply.github.com>
2023-03-31 18:30:44 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MyNavbar from "./components/MyNavbar";
+import MyFooter from "./components/MyFooter";
+import Home from "./components/Home";
+
+
+function App() {
+  return (
+    <div className="App">
+      <MyNavbar />
+      <Home />
+      <MyFooter />
+    </div>
+  );
+}
+
+export default App;
